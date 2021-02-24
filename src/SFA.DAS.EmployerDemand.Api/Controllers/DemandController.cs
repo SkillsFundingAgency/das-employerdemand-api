39b868072e67518bd4a47cba7b247b0d5db7350b
Scott Wakefield <scott@expertcc.co.uk>
2021-02-24 14:09:34 +0000

    AEDB-7 added basic controller.

---
+++ src/SFA.DAS.EmployerDemand.Api/Controllers/DemandController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.EmployerDemand.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]/")]
+    public class DemandController : ControllerBase
+    {
+        [HttpGet]
+        [Route("show")]
+        public IActionResult ShowDemand()
+        {
+            return Ok();
+        }
+    }
+}
