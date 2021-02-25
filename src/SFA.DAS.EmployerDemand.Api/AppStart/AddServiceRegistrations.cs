19e82f2ea66c75bea5b65c66249f7856cb1c0241
Scott Wakefield <scott@expertcc.co.uk>
2021-02-25 14:43:44 +0000

    AEDB-7 add startup config.

---
+++ src/SFA.DAS.EmployerDemand.Api/AppStart/AddServiceRegistrations.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SFA.DAS.Api.Common.Infrastructure;
+using SFA.DAS.Api.Common.Interfaces;
+
+namespace SFA.DAS.EmployerDemand.Api.AppStart
+{
+    public static class AddServiceRegistrations
+    {
+        public static void AddServiceRegistration(this IServiceCollection services, bool isDev)
+        {
+            services.AddTransient<IAzureClientCredentialHelper, AzureClientCredentialHelper>();
+        }
+    }
+}
