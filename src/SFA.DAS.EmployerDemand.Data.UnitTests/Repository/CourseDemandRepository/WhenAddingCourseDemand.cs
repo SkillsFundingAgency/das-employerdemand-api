27f298ccb8b54dffbd6ef0189573774471633b87
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-03-19 16:22:34 +0000

    Add create course demand item repository method

---
+++ src/SFA.DAS.EmployerDemand.Data.UnitTests/Repository/CourseDemandRepository/WhenAddingCourseDemand.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmployerDemand.Data.UnitTests.DatabaseMock;
+using SFA.DAS.EmployerDemand.Domain.Entities;
+
+namespace SFA.DAS.EmployerDemand.Data.UnitTests.Repository.CourseDemandRepository
+{
+    public class WhenAddingCourseDemand
+    {
+        private Mock<IEmployerDemandDataContext> _employerDemandDataContext;
+        private Data.Repository.CourseDemandRepository _courseDemandRepository;
+        private CourseDemand _courseDemandItem;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _courseDemandItem = 
+                new CourseDemand()
+                {
+                    Id = Guid.NewGuid(),
+                    DateCreated = DateTime.Now
+                };
+
+            _employerDemandDataContext = new Mock<IEmployerDemandDataContext>();
+            _employerDemandDataContext.Setup(x => x.CourseDemands).ReturnsDbSet(new List<CourseDemand>());
+            _courseDemandRepository = new Data.Repository.CourseDemandRepository(Mock.Of<ILogger<Data.Repository.CourseDemandRepository>>(), _employerDemandDataContext.Object);
+        }
+
+        [Test]
+        public async Task Then_The_CourseDemand_Item_Is_Added()
+        {
+            //Act
+            await _courseDemandRepository.Insert(_courseDemandItem);
+            
+            //Assert
+            _employerDemandDataContext.Verify(x=>x.CourseDemands.AddAsync(_courseDemandItem, It.IsAny<CancellationToken>()), Times.Once);
+            _employerDemandDataContext.Verify(x=>x.SaveChanges(), Times.Once);
+        }
+
+        [Test]
+        public async Task Then_If_The_Is_A_Constraint_Exception_It_Is_Handled()
+        {
+            //Arrange
+            _employerDemandDataContext.Setup(x => x.SaveChanges()).Throws(new DbUpdateException());
+            
+            //Act
+            await _courseDemandRepository.Insert(_courseDemandItem);
+            
+            //Assert
+            _employerDemandDataContext.Verify(x=>x.SaveChanges(), Times.Once);
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerDemand.Data/Repository/CourseDemandRepository.cs
@@ -0,0 +1,31 @@
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.EmployerDemand.Domain.Entities;
+
+namespace SFA.DAS.EmployerDemand.Data.Repository
+{
+    public class CourseDemandRepository
+    {
+        private readonly ILogger<CourseDemandRepository> _logger;
+        private readonly IEmployerDemandDataContext _dataContext;
+
+        public CourseDemandRepository (ILogger<CourseDemandRepository> logger, IEmployerDemandDataContext dataContext)
+        {
+            _logger = logger;
+            _dataContext = dataContext;
+        }
+        public async Task Insert(CourseDemand item)
+        {
+            try
+            {
+                await _dataContext.CourseDemands.AddAsync(item);
+                _dataContext.SaveChanges();
+            }
+            catch (DbUpdateException e)
+            {
+                _logger.LogInformation(e, "Unable to add course demand item");
+            }
+        }
+    }
+}
