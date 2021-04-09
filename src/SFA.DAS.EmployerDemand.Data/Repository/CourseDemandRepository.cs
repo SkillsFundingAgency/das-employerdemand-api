7556080b660b46bb9ba725185e4fda238ce17f19
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-04-09 11:37:20 +0100

    AEDB-26 - Add order by coursetitle

--- src/SFA.DAS.EmployerDemand.Data/Repository/CourseDemandRepository.cs
+++ src/SFA.DAS.EmployerDemand.Data/Repository/CourseDemandRepository.cs
@@ -84,7 +84,8 @@ namespace SFA.DAS.EmployerDemand.Data.Repository
                             courseId,
                             geography::Point(isnull(Lat,0), isnull(Long,0), 4326).STDistance(geography::Point(isnull({lat},0), isnull({lon},0), 4326)) * 0.0006213712 as DistanceInMiles
                         from CourseDemand) as dist on dist.Id = cd.Id
-                    Group by cd.CourseId, dist.DistanceInMiles ) derv on derv.CourseId = c.CourseId";
+                    Group by cd.CourseId, dist.DistanceInMiles ) derv on derv.CourseId = c.CourseId 
+                    Order by c.CourseTitle";
         }
     }
 }
