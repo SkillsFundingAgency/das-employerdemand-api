17253d6e0d4ce3cd3b94f20b341a673d4df9ac50
Scott Wakefield <scott@expertcc.co.uk>
2021-05-05 17:19:00 +0100

    AEDB-34 adding service.

---
+++ src/SFA.DAS.EmployerDemand.Application.UnitTests/ProviderInterest/Services/WhenCreatingProviderInterest.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmployerDemand.Application.ProviderInterest.Services;
+using SFA.DAS.EmployerDemand.Domain.Interfaces;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.EmployerDemand.Application.UnitTests.ProviderInterest.Services
+{
+    public class WhenCreatingProviderInterest
+    {
+        [Test, MoqAutoData]
+        public async Task Then_The_Repository_Is_Called(
+            bool returnValue,
+            Domain.Models.ProviderInterest providerInterest, 
+            [Frozen] Mock<IProviderInterestRepository> mockRepository,
+            ProviderInterestService service)
+        {
+            Domain.Entities.ProviderInterest param = null;
+            mockRepository
+                .Setup(repository => repository.Insert(
+                    It.IsAny<Domain.Entities.ProviderInterest>()))
+                .ReturnsAsync(returnValue)
+                .Callback((Domain.Entities.ProviderInterest interest) => param = interest);
+            
+            var actual = await service.CreateInterest(providerInterest);
+
+            param!.Should().BeEquivalentTo(providerInterest);
+            actual.Should().Be(returnValue);
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerDemand.Application/ProviderInterest/Services/ProviderInterestService.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using SFA.DAS.EmployerDemand.Domain.Interfaces;
+
+namespace SFA.DAS.EmployerDemand.Application.ProviderInterest.Services
+{
+    public class ProviderInterestService : IProviderInterestService
+    {
+        private readonly IProviderInterestRepository _providerInterestRepository;
+
+        public ProviderInterestService(IProviderInterestRepository providerInterestRepository)
+        {
+            _providerInterestRepository = providerInterestRepository;
+        }
+
+        public async Task<bool> CreateInterest(Domain.Models.ProviderInterest providerInterest)
+        {
+            return await _providerInterestRepository.Insert(providerInterest);
+        }
+    }
+}
