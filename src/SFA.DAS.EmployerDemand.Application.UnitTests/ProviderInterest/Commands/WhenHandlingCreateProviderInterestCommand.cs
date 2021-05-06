881c44e0a9df2309e856a698ae5f4e8ee449d68f
Scott Wakefield <scott@expertcc.co.uk>
2021-05-06 09:35:18 +0100

    AEDB-34 added handler.

--- src/SFA.DAS.EmployerDemand.Application.UnitTests/ProviderInterest/Commands/WhenHandlingCreateProviderInterestCommand.cs
+++ src/SFA.DAS.EmployerDemand.Application.UnitTests/ProviderInterest/Commands/WhenHandlingCreateProviderInterestCommand.cs
@@ -1,7 +1,63 @@
-﻿namespace SFA.DAS.EmployerDemand.Application.UnitTests.ProviderInterest.Commands
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmployerDemand.Application.ProviderInterest.Commands;
+using SFA.DAS.EmployerDemand.Domain.Interfaces;
+using SFA.DAS.Testing.AutoFixture;
+using ValidationResult = SFA.DAS.EmployerDemand.Domain.Validation.ValidationResult;
+
+namespace SFA.DAS.EmployerDemand.Application.UnitTests.ProviderInterest.Commands
 {
     public class WhenHandlingCreateProviderInterestCommand
     {
-        
+        [Test, MoqAutoData]
+        public void And_Command_Not_Valid_Then_ValidationException_Is_Thrown(
+            string propertyName,
+            CreateProviderInterestCommand command,
+            [Frozen] Mock<IValidator<CreateProviderInterestCommand>> mockValidator,
+            [Frozen] Mock<IProviderInterestService> mockService,
+            CreateProviderInterestCommandHandler handler)
+        {
+            //Arrange
+            mockValidator
+                .Setup(x=>x.ValidateAsync(command))
+                .ReturnsAsync(new ValidationResult{ValidationDictionary = { {propertyName,""}}});
+            
+            //Act
+            var act = new Func<Task>(async () => await handler.Handle(command, CancellationToken.None));
+
+            //Assert
+            act.Should().Throw<ValidationException>()
+                .WithMessage($"*{propertyName}*");
+            mockService.Verify(x=>x.CreateInterest(It.IsAny<Domain.Models.ProviderInterest>()), Times.Never);
+        }
+
+        [Test, MoqAutoData]
+        public async Task And_Command_Valid_Then_Service_Called(
+            bool result,
+            CreateProviderInterestCommand command,
+            [Frozen] Mock<IValidator<CreateProviderInterestCommand>> mockValidator,
+            [Frozen] Mock<IProviderInterestService> mockService,
+            CreateProviderInterestCommandHandler handler)
+        {
+            mockValidator
+                .Setup(x => x.ValidateAsync(command))
+                .ReturnsAsync(new ValidationResult());
+            mockService
+                .Setup(x=>x.CreateInterest(command.ProviderInterest))
+                .ReturnsAsync(result);
+            
+            var actual = await handler.Handle(command, CancellationToken.None);
+            
+            
+            mockValidator.Verify(x=>x.ValidateAsync(It.IsAny<CreateProviderInterestCommand>()), Times.Once);
+            actual.Id.Should().Be(command.ProviderInterest.Id);
+            actual.IsCreated.Should().Be(result);
+        }
     }
 }
--- src/SFA.DAS.EmployerDemand.Application/ProviderInterest/Commands/CreateProviderInterestCommandHandler.cs
+++ src/SFA.DAS.EmployerDemand.Application/ProviderInterest/Commands/CreateProviderInterestCommandHandler.cs
@@ -1,13 +1,39 @@
-﻿using System.Threading;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
+using SFA.DAS.EmployerDemand.Domain.Interfaces;
 
 namespace SFA.DAS.EmployerDemand.Application.ProviderInterest.Commands
 {
-    public class CreateProviderInterestCommandHandler : IRequestHandler<CreateProviderInterestCommand, CreateProviderInterestCommandResult> {
+    public class CreateProviderInterestCommandHandler : IRequestHandler<CreateProviderInterestCommand, CreateProviderInterestCommandResult> 
+    {
+        private readonly IProviderInterestService _service;
+        private readonly IValidator<CreateProviderInterestCommand> _validator;
+
+        public CreateProviderInterestCommandHandler(
+            IProviderInterestService service, 
+            IValidator<CreateProviderInterestCommand> validator)
+        {
+            _service = service;
+            _validator = validator;
+        }
+
         public async Task<CreateProviderInterestCommandResult> Handle(CreateProviderInterestCommand request, CancellationToken cancellationToken)
         {
-            throw new System.NotImplementedException();
+            var validationResult = await _validator.ValidateAsync(request);
+
+            if (!validationResult.IsValid())
+            {
+                throw new ValidationException(validationResult.DataAnnotationResult,null, null);
+            }
+            
+            var result = await _service.CreateInterest(request.ProviderInterest);
+            return new CreateProviderInterestCommandResult
+            {
+                Id = request.ProviderInterest.Id,
+                IsCreated = result
+            };
         }
     }
 }
