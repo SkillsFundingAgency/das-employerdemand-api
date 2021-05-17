7eab466131d1169b0c5d8fcf16a114980443d985
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-05-17 16:39:45 +0100

    Add handler for getting demand

---
+++ src/SFA.DAS.EmployerDemand.Application/CourseDemand/Queries/GetCourseDemand/GetCourseDemandQueryResult.cs
@@ -0,0 +1,7 @@
+namespace SFA.DAS.EmployerDemand.Application.CourseDemand.Queries.GetCourseDemand
+{
+    public class GetCourseDemandQueryResult
+    {
+        public Domain.Models.CourseDemand CourseDemand { get ; set ; }
+    }
+}
