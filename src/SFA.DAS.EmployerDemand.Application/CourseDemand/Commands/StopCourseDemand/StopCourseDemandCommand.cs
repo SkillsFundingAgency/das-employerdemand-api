2710177b461ddbb8625c259cb697f8f0650fbae3
Scott Wakefield <scott@expertcc.co.uk>
2021-06-09 12:08:06 +0100

    AEDB-46 handler to stop demand.

---
+++ src/SFA.DAS.EmployerDemand.Application/CourseDemand/Commands/StopCourseDemand/StopCourseDemandCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using MediatR;
+
+namespace SFA.DAS.EmployerDemand.Application.CourseDemand.Commands.StopCourseDemand
+{
+    public class StopCourseDemandCommand : IRequest<StopCourseDemandCommandResult>
+    {
+        public Guid Id { get; set; }
+    }
+}
+
